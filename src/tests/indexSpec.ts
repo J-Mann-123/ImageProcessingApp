e50d79fe09070fb0cdc2a40defab91d664583684
J-Mann-123 <jmann5326@gmail.com>
2023-03-22 17:21:59 +1030

    added comments to test files

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,22 +2,30 @@ import fs from 'fs'
 
 import fetch from 'isomorphic-fetch'
 
+// check if the unedited image exists
 it('Should expect JohnWick.jpg to exist', () => {
     expect(fs.existsSync('src/images/JohnWick.jpg')).toBeTruthy()
 })
 
+// check if the edited image exists
 it('Should expect edited-JohnWick.jpg to exist', () => {
     expect(fs.existsSync('src/thumbs/edited-JohnWick.jpg')).toBeTruthy()
 })
 
-// this should return true only after npm run start has been run
+// this should pass only after npm run start has been run
 it('Should return false for an invalid link', async () => {
     const response = await fetch('http://localhost:3000/images/JohnWick.jpg')
     expect(response).toBeTruthy()
 })
 
-// this should return true only after npm run start has been run
+// this should pass only after npm run start has been run
 it('Should return false for an invalid link', async () => {
     const response = await fetch('http://localhost:3000/thumbs/edited-JohnWick.jpg')
+    expect(response).toBeTruthy()
+})
+
+// This should pass if the resizable link exists and after npm run start has been run
+it('Should return false for an invalid link', async () => {
+    const response = await fetch('http://localhost:3000/resize?imageUrl=https://localhost:3000/images/JohnWick.jpg&width=340&height=380')
     expect(response).toBeTruthy()
 })
