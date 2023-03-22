a0b09d6a1dfd6cf2f0605b7752060454d0dace14
J-Mann-123 <jmann5326@gmail.com>
2023-03-22 20:11:19 +1030

    Revert "fixed prettier and eslint issues"
    This reverts commit 0386c96268c28d7ef898a36022b82530bac4dd2b.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    "prettier"
-  ],
+  extends: 'standard-with-typescript',
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
