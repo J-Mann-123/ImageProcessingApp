0b301a63ad195b2067b2891b618afade5df692c3
J-Mann-123 <jmann5326@gmail.com>
2023-03-22 10:53:34 +1030

    wrote some tests

--- build/tests/indexSpec.js
+++ build/tests/indexSpec.js
@@ -1,5 +1,19 @@
 "use strict";
 // export default myFunc;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // it('expect myFunc(5) to equal 25', () => {
-//   expect(myFunc(5)).toEqual(25);
+//     expect(myFunc(5)).toEqual(25);
 // });
+const fs_1 = __importDefault(require("fs"));
+it('Should expect JohnWick.jpg to exist', () => {
+    expect(fs_1.default.existsSync('src/images/JohnWick.jpg')).toBeTruthy();
+});
+it('Should expect edited-JohnWick.jpg to exist', () => {
+    expect(fs_1.default.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy();
+});
+it('should expect JohnWick.jpg to exist', () => {
+    expect(fs_1.default.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy();
+});
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,19 @@
 // export default myFunc;
 
 // it('expect myFunc(5) to equal 25', () => {
-//   expect(myFunc(5)).toEqual(25);
+//     expect(myFunc(5)).toEqual(25);
 // });
+
+import fs from 'fs'
+
+it('Should expect JohnWick.jpg to exist', () => {
+    expect(fs.existsSync('src/images/JohnWick.jpg')).toBeTruthy()
+})
+
+it('Should expect edited-JohnWick.jpg to exist', () => {
+    expect(fs.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy()
+})
+
+// it('should expect JohnWick.jpg to exist', () => {
+//     expect(fs.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy()
+// })
