ca512064104b0a289282dea134ac58e7415fc0f6
J-Mann-123 <jmann5326@gmail.com>
2023-03-22 14:12:14 +1030

    installed packages like updated npm and node-fetch

--- build/tests/indexSpec.js
+++ build/tests/indexSpec.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
 it('Should expect JohnWick.jpg to exist', () => {
     expect(fs_1.default.existsSync('src/images/JohnWick.jpg')).toBeTruthy();
 });
 it('Should expect edited-JohnWick.jpg to exist', () => {
     expect(fs_1.default.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy();
+});
+it('Should return true for a valid link', async () => {
+    const response = await (0, node_fetch_1.default)('http://localhost:3000/images/JohnWick.jpg');
+    expect(response.ok).toBe(true);
 });
 // it('should expect JohnWick.jpg to exist', () => {
 //     expect(fs.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy()
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import fetch from 'node-fetch';
 
 it('Should expect JohnWick.jpg to exist', () => {
     expect(fs.existsSync('src/images/JohnWick.jpg')).toBeTruthy()
@@ -6,6 +7,11 @@ it('Should expect JohnWick.jpg to exist', () => {
 
 it('Should expect edited-JohnWick.jpg to exist', () => {
     expect(fs.existsSync('src/images/edited-JohnWick.jpg')).toBeTruthy()
+})
+
+it('Should return true for a valid link', async () => {
+    const response = await fetch('http://localhost:3000/images/JohnWick.jpg')
+    expect(response.ok).toBe(true)
 })
 
 // it('should expect JohnWick.jpg to exist', () => {
