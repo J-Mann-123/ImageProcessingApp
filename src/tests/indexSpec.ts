3542f5457aed8160c307f62ff8b1d4399f3019a6
J-Mann-123 <jmann5326@gmail.com>
2023-03-22 20:13:01 +1030

    Revert "downloaded @typescript-eslint/parser"
    This reverts commit f5966e69e64743278272140872e369483d56fd0e.

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,35 +1,35 @@
-import fs from 'fs';
+import fs from 'fs'
 
-import fetch from 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch'
 
 // check if the unedited image exists
 it('Should expect JohnWick.jpg to exist', () => {
-  expect(fs.existsSync('src/images/JohnWick.jpg')).toBeTruthy();
-});
+  expect(fs.existsSync('src/images/JohnWick.jpg')).toBeTruthy()
+})
 
 // check if the edited image exists
 it('Should expect edited-JohnWick.jpg to exist', () => {
-  expect(fs.existsSync('src/thumbs/edited-JohnWick.jpg')).toBeTruthy();
-});
+  expect(fs.existsSync('src/thumbs/edited-JohnWick.jpg')).toBeTruthy()
+})
 
 // this should pass only after npm run start has been run
 it('Should return false for an invalid link', async () => {
-  const response = await fetch('http://localhost:3000/images/JohnWick.jpg');
-  expect(response).toBeTruthy();
-});
+  const response = await fetch('http://localhost:3000/images/JohnWick.jpg')
+  expect(response).toBeTruthy()
+})
 
 // this should pass only after npm run start has been run
 it('Should return false for an invalid link', async () => {
   const response = await fetch(
     'http://localhost:3000/thumbs/edited-JohnWick.jpg'
-  );
-  expect(response).toBeTruthy();
-});
+  )
+  expect(response).toBeTruthy()
+})
 
 // This should pass if the resizable link exists and after npm run start has been run
 it('Should return false for an invalid link', async () => {
   const response = await fetch(
     'http://localhost:3000/resize?imageUrl=https://localhost:3000/images/JohnWick.jpg&width=340&height=380'
-  );
-  expect(response).toBeTruthy();
-});
+  )
+  expect(response).toBeTruthy()
+})
