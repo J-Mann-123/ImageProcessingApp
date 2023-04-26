295ea190b3a5a6725fb0f8e0a18c6c8188004dfc
J-Mann-123 <jmann5326@gmail.com>
2023-04-26 14:02:13 +0930

    build

--- build/index.js
+++ build/index.js
@@ -1,42 +1,31 @@
-'use strict'
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod }
-  }
-Object.defineProperty(exports, '__esModule', { value: true })
-const express_1 = __importDefault(require('express'))
-const index_1 = __importDefault(require('./routes/index'))
-const path_1 = __importDefault(require('path'))
-const sharpFunction_1 = require('./routes/sharpFunction')
-const app = (0, express_1.default)()
-const port = 3000
-app.use('/api', index_1.default)
-app.use(
-  '/images',
-  express_1.default.static(path_1.default.join(__dirname, 'images'))
-)
-app.use(
-  '/thumbs',
-  express_1.default.static(path_1.default.join(__dirname, 'thumbs'))
-)
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const index_1 = __importDefault(require("./routes/index"));
+const path_1 = __importDefault(require("path"));
+const sharpFunction_1 = require("./routes/sharpFunction");
+const app = (0, express_1.default)();
+const port = 3000;
+app.use('/api', index_1.default);
+app.use('/images', express_1.default.static(path_1.default.join(__dirname, 'images')));
+app.use('/thumbs', express_1.default.static(path_1.default.join(__dirname, 'thumbs')));
 app.get('/resize', async (req, res) => {
-  const imageUrl = 'src/images/JohnWick.jpg'
-  const width = parseInt(req.query.width)
-  const height = parseInt(req.query.height)
-  try {
-    const buffer = await (0, sharpFunction_1.resizeImage)(
-      imageUrl,
-      width,
-      height
-    )
-    res.set('Content-Type', 'image/jpeg')
-    res.send(buffer)
-  } catch (err) {
-    console.error(err)
-    res.status(200).send('Internal server error')
-  }
-})
+    const imageUrl = 'src/images/JohnWick.jpg';
+    const width = parseInt(req.query.width);
+    const height = parseInt(req.query.height);
+    try {
+        const buffer = await (0, sharpFunction_1.resizeImage)(imageUrl, width, height);
+        res.set('Content-Type', 'image/jpeg');
+        res.send(buffer);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(200).send('Internal server error');
+    }
+});
 app.listen(port, () => {
-  console.log(`server started at localhost:${port}`)
-})
+    console.log(`server started at localhost:${port}`);
+});
--- build/routes/sharpFunction.js
+++ build/routes/sharpFunction.js
@@ -1,18 +1,14 @@
-'use strict'
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod }
-  }
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.resizeImage = void 0
-const sharp_1 = __importDefault(require('sharp'))
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resizeImage = void 0;
+const sharp_1 = __importDefault(require("sharp"));
 const resizeImage = async (imageUrl, width, height) => {
-  const buffer = await (0, sharp_1.default)(imageUrl)
-    .resize(width, height)
-    .toBuffer()
-  // Save resized image to file
-  await (0, sharp_1.default)(buffer).toFile('src/thumbs/edited-JohnWick.jpg')
-  return buffer
-}
-exports.resizeImage = resizeImage
+    const buffer = await (0, sharp_1.default)(imageUrl).resize(width, height).toBuffer();
+    // Save resized image to file
+    await (0, sharp_1.default)(buffer).toFile('src/thumbs/edited-JohnWick.jpg');
+    return buffer;
+};
+exports.resizeImage = resizeImage;
